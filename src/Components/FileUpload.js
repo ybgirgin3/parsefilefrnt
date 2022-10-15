da1fcf315468cd257af7151f4261d43440b9e692
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-10-15 18:48:21 +0300

    loading animation on button while waiting response from api

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -79,15 +79,33 @@ const FileUpload = () => {
         <div>
           {isresp200 ? (
             <button
-              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+              className="bg-blue-500 text-white font-bold py-2 px-4 border-b-4 border-blue-700 rounded"
               onClick={handleSubmission}>
               Parse
             </button>
           ) : (
             <button
-              className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-              onClick={handleSubmission}>
-              Processing, Please wait..
+              type="button"
+              class="inline-flex items-center px-4 py-2 text-sm font-semibold leading-6 text-white transition duration-150 ease-in-out bg-blue-500 rounded-md shadow cursor-not-allowed hover:bg-indigo-400"
+              disabled="">
+              <svg
+                class="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24">
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+              Processing...
             </button>
           )}
         </div>
