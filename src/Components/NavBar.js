8a3290258e0690622b758d5c1801ea2ac47c4d22
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-10-16 09:36:14 +0300

    basic navbar for `BETA` tag

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <div className='"w-screen p-3 text-white bg-gradient-to-tr from-red-900 via-red-500 to-yellow-500 shadow fixed'>
+      BETA
+    </div>
+  );
+}
+
+export default NavBar;
