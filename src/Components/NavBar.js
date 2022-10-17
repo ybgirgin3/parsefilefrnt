c57e58667471a08974bf1850e2aca37a802397d8
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-10-17 21:59:07 +0300

    how it works url will be implementing..

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function NavBar() {
   return (
+    // <a href="#" class="no-underline hover:underline ...">Link</a>
+
     <div className='"w-screen p-3 text-white bg-gradient-to-tr from-red-900 via-red-500 to-yellow-500 shadow fixed'>
-      BETA
+      BETA | 
+      <a href="/#" className="no-underline hover:underline">
+        How It Works?
+      </a>
     </div>
   );
 }
