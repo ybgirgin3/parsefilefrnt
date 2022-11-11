5c5167214ce8bab457785f0ccb056ad4911aed88
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-11-11 19:58:40 +0300

    urls are on environment gitignore updated readme updated

--- src/Components/index.js
+++ src/Components/index.js
@@ -6,13 +6,11 @@ import 'antd/dist/antd.min.css';
 
 const Assembly = () => {
   // * variables
-  // let local_url = 'http://localhost:8080';
-  let heroku_url = 'https://parsefileapi.herokuapp.com/uploadfile/';
 
   return (
     <Tabs defaultActiveKey="1" centered>
       <Tabs.TabPane tab="App" key="1">
-        <UploadAndParse url={heroku_url} />
+        <UploadAndParse url={process.env.REACT_APP_HEROKU_URL} />
       </Tabs.TabPane>
       <Tabs.TabPane tab="How It Works?" key="2">
         <HowItWorks />
