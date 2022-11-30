b62b191c8f57443ae9cc22860f7a852da137ee23
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-11-30 10:44:06 +0300

    env vars added

--- src/Components/index.js
+++ src/Components/index.js
@@ -4,11 +4,14 @@ import { Tabs } from 'antd';
 import HowItWorks from './HowItWorks';
 import 'antd/dist/antd.min.css';
 
+// env 
+const { REACT_APP_HEROKU_URL } = process.env
+
 const Assembly = () => {
   return (
     <Tabs defaultActiveKey="1" centered>
       <Tabs.TabPane tab="App" key="1">
-        <UploadAndParse url="https://parsefileapi.herokuapp.com/uploadfile/" />
+        <UploadAndParse url={{ REACT_APP_HEROKU_URL }} />
       </Tabs.TabPane>
       <Tabs.TabPane tab="How It Works?" key="2">
         <HowItWorks />
