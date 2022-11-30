5878a7021e6deace0002bf09978f10a1d244cd1b
Yusuf Berkay Girgin <ybgirgin3@gmail.com>
2022-11-30 19:25:45 +0300

    data preview prettier'd, download button fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import React from 'react';
 import NavBar from './Components/NavBar';
 import Assembly from './Components';
-import Footerx from './Components/Footerx';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Assembly />
-      <Footerx />
     </div>
   );
 }
--- src/Components/DataPreview.js
+++ src/Components/DataPreview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Empty } from 'antd';
+import Preview from 'react-data-preview';
 
 const DataPreview = ({ response }) => {
   // extract data
   const _data = response.data;
 
   return (
+    <div>{_data ? <Preview theme={'light'} data={_data} /> : <Empty />}</div>
+    /*
     <div>
       {_data ? (
         <code>
@@ -15,6 +18,7 @@ const DataPreview = ({ response }) => {
         <Empty />
       )}
     </div>
+    */
   );
 };
 
